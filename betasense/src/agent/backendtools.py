076eb1d45b5654c3701f6ec505bb2077ac2a4ee9
ruikai30 <liuruikai30@gmail.com>
2025-10-25 15:03:53 +0100

    HOTO commit

--- betasense/src/agent/backendtools.py
+++ betasense/src/agent/backendtools.py
@@ -29,4 +29,65 @@ def tool_1(args) -> str:
     pass
 
 
+# def retrieve_playbook(playbook_name: str) -> str:
+#     """
+#     Choose the right playbook based on the user's request.
+#     """
+#     if playbook_name == "long_term_value_investor":
+#         return LONG_TERM_VALUE_INVESTOR_PLAYBOOK
+#     elif playbook_name == "short_term_swing_trade":
+#         return SHORT_TERM_SWING_TRADE_PLAYBOOK
+#     elif playbook_name == "merger_arbitrage":
+#         return MERGER_ARBITRAGE_PLAYBOOK
+#     elif playbook_name == "carve_outs":
+#         return CARVE_OUTS_PLAYBOOK
+#     elif playbook_name == "growth_investor":
+#         return GROWTH_INVESTOR_PLAYBOOK
+#     elif playbook_name == "net_net_cigar_butt":
+
+
 # TODO: Bryan to add more tools here
+
+"""
+Information toolbox:
+- search web
+- earning transcript #
+- investor presentation *
+- press release #
+- expert transcripts #
+- euromonitor
+- financials
+- 10k *
+- sell side research *
+- comps
+- insider transactions
+- insider ownership
+- performance-based compensation
+
+Perspective toolbox:
+- long term value investor
+- short term swing trade
+- merger arbitrage
+- carve outs
+- growth investor
+- net net cigar butt
+- activist investor
+
+Industry toolbox:
+- TMT
+- Industrials
+- Consumer Discretionary
+- Consumer Staples
+- Energy
+- Financials
+- Health Care
+- Industrials
+- Materials
+- Utilities
+
+Utils toolbox:
+- Parse pdf
+- Parse excel
+- Read image
+- Think...
+"""
--- betasense/src/prompts/playbooks.py
+++ betasense/src/prompts/playbooks.py
@@ -1,4 +1,4 @@
-PLAYBOOK_1 = """
+LONG_TERM_VALUE_INVESTOR_PLAYBOOK = """
 Playbook for e.g. how to read a stock market report:
 
 500 word essay of domain knowledge on xxx

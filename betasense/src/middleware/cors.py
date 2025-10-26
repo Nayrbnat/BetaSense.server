0b8263dffb9c92d50eca669adb7083c35bdeb13e
ruikai30 <liuruikai30@gmail.com>
2025-10-26 17:34:16 +0000

    updated cors

--- betasense/src/middleware/cors.py
+++ betasense/src/middleware/cors.py
@@ -5,21 +5,8 @@ from fastapi.middleware.cors import CORSMiddleware
 def setup_cors(app: FastAPI):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[
-            "https://beta-sense-frontend.vercel.app/",
-
-            "http://localhost:1000",
-            "http://localhost:2000",
-            "http://localhost:3000",
-            "http://localhost:4000",
-            "http://localhost:5000",
-            "http://localhost:6000",
-            "http://localhost:7000",
-            "http://localhost:8000",
-            "http://localhost:9000",
-            "http://localhost:10000",
-        ],
-        allow_credentials=True,
+        allow_origins=["*"],
+        allow_credentials=False,
         allow_methods=["*"],
         allow_headers=["*"],
     )
